49d931f213956a1fe292188e96ead037ba091f22
yoonhee <newyoonhee@gmail.com>
2023-08-18 18:00:24 +0900

    장바구니 구현

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -4,29 +4,59 @@ import Header from '../component/Header';
 import Footer from '../component/Footer';
 import Nav from '../component/Nav';
 
-const Cart = () => {
-  return(
+const Cart = ({ food, convertPrice }) => {
+
+  let list = JSON.parse(sessionStorage.getItem('item'));
+  let totalPrice= list.reduce((acc, temp)=>
+  acc + temp.price * temp.count);
+
+  
+  console.log(totalPrice);
+  
+  return (
+
+  
     <>
-    <Header />
-    <Nav />
-    <div className='Cart'>
-      <div>장바구니</div>
-      <hr />
-      <Table>
-        <thead>
-          <tr>
-            <td><input type='checkbox' name='allcheck'/></td>
-            <td>상품/옵션정보</td>
-            <td>수량</td>
-            <td>상품금액</td>
-          </tr>
-        </thead>
-        <tbody>
-          
-        </tbody>
-      </Table>  
-    </div>
-    <Footer />
+      <Header />
+      <Nav />
+      <div className='Cart'>
+        <div className='bigTitle'>장바구니</div>
+        <hr />
+        <Table>
+          <thead>
+            <tr>
+              <td className='smallTitle'><input type='checkbox' name='allCheck' /></td>
+              <td className='smallTitle'>상품</td>
+              <td className='smallTitle'>옵션정보</td>
+              <td className='smallTitle'>수량</td>
+              <td className='smallTitle'> 상품금액</td>
+            </tr>
+          </thead>
+          <tbody>
+              {
+                list.map((data) => {
+                  return(
+                    <tr className='list'>
+                    <td><input type='checkbox' name='Check'/></td>
+                    <td><img src={food[data.id].img}/></td>
+                    <td>{food[data.id].title}</td>
+                    <td>{data.count}</td>
+                    <td>{convertPrice(food[data.id].price*data.count)}원</td>
+                    </tr>
+                  )
+                })
+              }
+          </tbody>
+        </Table>
+        <div>
+            <div>총 상품금액</div>
+            <div>{convertPrice(totalPrice)}원</div>
+            <div>
+              <button>상품주문</button>
+            </div>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 }
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -8,16 +8,52 @@ import { useState } from 'react';
 const Detail = ({ food, convertPrice }) => {
   const navigate = useNavigate();
   const { id } = useParams();
+  // 수량 state
   const [count, setCount] = useState(1);
 
+
+  // 수량 +/- 함수
   const addSub = (type) => {
-    if(type === 'add') {
+    if (type === 'add') {
       setCount(count + 1)
     } else {
-      if(count === 1) return;
+      if (count === 1) return;
       setCount(count - 1)
     }
   }
+
+  const itemCart = (id, count) => {
+
+    // let item = sessionStorage.getItem('item');
+    // item = JSON.parse(item);
+  
+
+    if(sessionStorage.getItem('item')==null) {
+      // 상품이 장바구니에 없을 경우
+      sessionStorage.setItem('item', JSON.stringify([{'id': id, 'count': count}]));
+    } else {
+      // 중복된 상품이 있는 경우
+      let temp = JSON.parse(sessionStorage.getItem('item'));
+      let index;  // 배열위치 저장변수
+      let isHave; // 세션스토리지에 있냐, 없냐 저장 변수
+
+      temp.forEach((data, i) => {
+        if(id == data.id) {
+          index = i;
+          isHave = true;
+        }
+      });
+
+      if(isHave) {
+        temp[index].count += count;
+      } else {
+        temp.push({'id': id, 'count': count});
+      }
+
+      sessionStorage.setItem('item', JSON.stringify(temp));
+    }
+  }
+
 
   return (
     <>
@@ -32,47 +68,30 @@ const Detail = ({ food, convertPrice }) => {
             <div>
               <p className='title'>{food[id].title}</p>
               <p className='price'>가격: {convertPrice(food[id].price)}원</p>
-              <p className='cnt'>수량: 
-              <button onClick={() => {
-                addSub('sub');
-              }}>-</button>
-              {count}
-              <button onClick={() => {
-                addSub('add');
-              }}>+</button>
+              <p className='cnt'>수량:
+                <button onClick={() => {
+                  addSub('sub');
+                }}>-</button>
+                {count}
+                <button onClick={() => {
+                  addSub('add');
+                }}>+</button>
               </p>
               <hr />
               <p className='total'>총 가격: {convertPrice(Number(food[id].price) * count)}원</p>
             </div>
             <div className='btn'>
-            <button onClick={() => {
-              alert('장바구니에 추가 되었습니다.')
-            }}>장바구니</button>
-            <button onClick={() => {
-              navigate('/order');
-            }}>주문하기</button>
+              <button onClick={() => {
+                itemCart(food[id].id, count);
+                alert('장바구니에 추가 되었습니다.');
+                navigate('/cart');
+              }}>장바구니</button>
+              <button onClick={() => {
+                navigate('/order');
+              }}>주문하기</button>
             </div>
           </div>
         </div>
-        {/* 
-        <Nav className="mt-5" justify variant="tabs" defaultActiveKey="link-0">
-          <Nav.Item>
-            <Nav.Link eventKey="link-0" onClick={() => {
-
-            }}>상세정보</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link-1" onClick={() => {
-
-            }}>리뷰</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link-2" onClick={() => {
-
-            }}>반품,교환정보</Nav.Link>
-          </Nav.Item>
-        </Nav> */}
-
       </div>
       <Footer />
     </>
